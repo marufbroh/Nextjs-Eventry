1a3be012bee0d3825049996efa1c86a214671c69
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-15 15:44:48 +0600

    login done from client component

--- eventry/app/actions/index.js
+++ eventry/app/actions/index.js
@@ -1,14 +1,27 @@
 "use server";
 
-const { createUser } = require("@/db/queries");
+const { createUser, findUserByCredentials } = require("@/db/queries");
 const { redirect } = require("next/navigation");
 
 const registerUser = async (formData) => {
-    // console.log(formData);
+  // console.log(formData);
   const user = Object.fromEntries(formData); // array of array/obj ke obj banay
 
   const created = await createUser(user);
   redirect("/login");
 };
 
-export { registerUser };
+const performLogin = async (formData) => {
+  try {
+    const credentials = {};
+    credentials.email = formData.get("email");
+    credentials.password = formData.get("password");
+
+    const user = await findUserByCredentials(credentials);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { registerUser, performLogin };
--- eventry/db/queries.js
+++ eventry/db/queries.js
@@ -1,6 +1,9 @@
 import { eventModel } from "@/models/event-models";
 import { userModel } from "@/models/user-model";
-import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/utils/data-util";
+import {
+  replaceMongoIdInArray,
+  replaceMongoIdInObject,
+} from "@/utils/data-util";
 
 const getAllEvents = async () => {
   const allEvents = await eventModel.find().lean();
@@ -12,8 +15,18 @@ const getEventById = async (eventId) => {
   return replaceMongoIdInObject(event);
 };
 
-const createUser = async(user) => {
-return await userModel.create(user)
-}
+const createUser = async (user) => {
+  return await userModel.create(user);
+};
 
-export { getAllEvents, getEventById, createUser };
+const findUserByCredentials = async (credentials) => {
+  const user = await userModel.findOne(credentials).lean();
+
+  if (user) {
+    return replaceMongoIdInObject(user);
+  }
+
+  return null;
+};
+
+export { getAllEvents, getEventById, createUser, findUserByCredentials };
