ca9edbed96e12295de13642702809ae98e568a1e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-17 10:50:40 +0600

    Send Email functionality

--- eventry/app/actions/index.js
+++ eventry/app/actions/index.js
@@ -1,12 +1,15 @@
 "use server";
 
+import EmailTemplate from "@/components/payments/EmailTemplate";
 import { revalidatePath } from "next/cache";
+import { Resend } from "resend";
 
 const {
   createUser,
   findUserByCredentials,
   updateInterest,
   updateGoing,
+  getEventById,
 } = require("@/db/queries");
 const { redirect } = require("next/navigation");
 
@@ -43,12 +46,30 @@ const addInterestedEvent = async (eventId, authId) => {
 const addGoingEvent = async (eventId, user) => {
   try {
     await updateGoing(eventId, user?.id);
+    await sendEmail(eventId, user);
   } catch (error) {
     throw error;
   }
 
   revalidatePath("/");
-  redirect("/")
+  redirect("/");
 };
 
-export { registerUser, performLogin, addInterestedEvent, addGoingEvent };
+const sendEmail = async (eventId, user) => {
+  try {
+    const event = await getEventById(eventId);
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    const message = `Dear ${user?.name}, you have been successfully registered for the event, ${event?.name}. Please carry this email and your official id to the venue. We are excited to have you here.`;
+
+    const sent = await resend.emails.send({
+      from: "noreply@resend.com",
+      to: user?.email,
+      subject: "Successfully Registered for the event!",
+      react: EmailTemplate({ message }),
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { registerUser, performLogin, addInterestedEvent, addGoingEvent, sendEmail };
